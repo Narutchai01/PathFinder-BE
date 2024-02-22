a0c3ad27c983b673ceb879c06cbffe739673a6cb
Narutchai <koonfluk2015@gmail.com>
2024-02-23 00:07:10 +0700

    updatea

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World!")
+		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
-
+	app.Listen(":" + port)
 }
